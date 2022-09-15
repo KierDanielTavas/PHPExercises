5c841abd24db70fb459e2f17418695ce50f8ce2f
Kier Daniel Tavas <80656633+KierDanielTavas@users.noreply.github.com>
2022-09-15 21:12:44 +0800

    Php Activities(Lab)

---
+++ exercise_1.php
@@ -0,0 +1,11 @@
+ <?php
+        
+        echo "Twinkle, Twinkle little Star! ";
+        echo"\n";
+        $t = "Twinkle";
+        $s ="Star";
+        
+        echo " $t, $t little $s!  ";
+        
+   ?>
+  
---
+++ exercise_4.php
@@ -0,0 +1,18 @@
+	<?php   
+ 
+			$name='Harry'; 
+			$age=28;   
+ 
+			var_dump($name); 
+		 
+ 
+			print_r($name); 
+			echo "\n";   
+ 
+			var_dump($age); 
+		
+			$name=NULL;   
+			var_dump($name);
+
+		?> 
+
